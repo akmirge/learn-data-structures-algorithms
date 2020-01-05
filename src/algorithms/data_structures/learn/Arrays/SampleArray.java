aa9d802dfd2d97220def02f525bccd97eeccf9a8
anilkmirge <manilche@gmail.com>
2020-01-04 19:05:06 -0500

    Adding O(1) Constant time complexity example in Array

--- src/algorithms/data_structures/learn/Arrays/SampleArray.java
+++ src/algorithms/data_structures/learn/Arrays/SampleArray.java
@@ -3,7 +3,8 @@ package algorithms.data_structures.learn.Arrays;
 public class SampleArray {
   public static void main(String args[]) {
     int[] intArray = {12, 32, -15, 46, -35, 88, 9};
-    // If you need to find an index of an element say -15
+    // If you need to find an index of an element say 23 which doesn't exist leading to Big O(n) (Linear) time
+    // complexity
     int index = -1;
     for (int i = 0; i < intArray.length; i++) {
       if (intArray[i] == 23) {
@@ -12,5 +13,14 @@ public class SampleArray {
       }
     }
     System.out.println(index < 0 ? "Index not found" : "Index is: " + index);
+
+    //If you need to search an element in the Array with a given index say 2, it will lead to a Constant time
+    // O(1) complexity
+    index = 2;
+    if (index < 0 || index >= intArray.length) {
+      throw new RuntimeException("Index out of bound");
+    }
+    System.out.println("Element value is "+ intArray[index]);
+
   }
 }
