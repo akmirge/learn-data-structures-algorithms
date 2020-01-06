d8f75f3b108f444196ddd62b274a94d4a3bde9a0
anilkmirge <manilche@gmail.com>
2020-01-05 20:18:26 -0500

    Adding Array search algorithm with O(n^2) time complexity

---
+++ src/algorithms/data_structures/learn/arrays/searching/ArrayElements.java
@@ -0,0 +1,54 @@
+package algorithms.data_structures.learn.arrays.searching;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayElements {
+
+  class Pair {
+    int first, second;
+
+    Pair(int f, int s) {
+      this.first = f;
+      this.second = s;
+    }
+  }
+
+
+  public static void main(String args[]) {
+    ArrayElements elements = new ArrayElements();
+    int[] array = {6, 7, 4, 5, 12, 16, 1}; //two set pairs
+//  int[] array = {12, 10, 1, 3, 15}; // one set pairs
+//  int[] array = {6, 16, 8, 4, 15}; // no match
+
+
+    boolean pairsFound =	elements.findPairs(array);
+    if (!pairsFound) {
+      System.out.println("No pairs were found for the given array!");
+    }
+  }
+
+  public boolean findPairs(int[] array) {
+    if (array == null || array.length == 0) {
+      return false;
+    }
+    int sum;
+    Map<Integer, Pair> pairMap = new HashMap<Integer, Pair>();
+    boolean pairFound = false;
+    for (int i = 0; i < array.length; i++) {
+      for (int j = i + 1; j < array.length; j++) {
+        sum = array[i] + array[j];
+        if (!pairMap.containsKey(sum)) {
+          pairMap.put(sum, new Pair(i, j));
+        } else {
+          Pair p = pairMap.get(sum);
+          System.out.println("(" + array[p.first] + "," + array[p.second] + "), ("+ array[i] + "," + array[j] + ")");
+          pairFound  = true;
+        }
+      } // inner for loop
+
+    } //outer for loop
+
+    return pairFound;
+  }
+}
