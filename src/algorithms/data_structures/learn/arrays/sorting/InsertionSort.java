a7851a28071ef82bd11bc013392915d459214709
anilkmirge <manilche@gmail.com>
2020-01-07 14:25:50 -0500

    Adding Insertion Sort

---
+++ src/algorithms/data_structures/learn/arrays/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package algorithms.data_structures.learn.arrays.sorting;
+
+public class InsertionSort {
+  public static void main(String args[]) {
+    int[] array = {25, 32, -15, 67, -55, 19, 1};
+
+    for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
+      int newElement = array[firstUnsortedIndex];
+      int i;
+      for (i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
+        array[i] = array[i - 1];
+      }
+      array[i] = newElement;
+    }
+    for (int i = 0; i < array.length; i++) {
+      System.out.println(array[i]);
+    }
+
+  }
+}
